0c9d7b17d57ebe8c7dd00fec5a4e201ac5728fc6
Mensur Islami <mi@live.de>
2023-09-24 16:55:37 +0200

    minor

--- js/index.js
+++ js/index.js
@@ -1,18 +1,5 @@
+// copy text
 window.copyText = function (textToCopy) {
-
-
-    // var myTemporaryInputElement = document.createElement("input");
-    // myTemporaryInputElement.type = "text";
-    // myTemporaryInputElement.value = textToCopy;
-
-    // document.body.appendChild(myTemporaryInputElement);
-
-    // myTemporaryInputElement.select();
-    // myTemporaryInputElement.setSelectionRange(0, 99999); // For mobile devices
-
-    // Copy the text inside the text field
     navigator.clipboard.writeText(textToCopy);
-
     document.body.removeChild(myTemporaryInputElement);
-
 }
